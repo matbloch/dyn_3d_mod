f7a05fdbace163184be1de843c20eb24801d6af2
Matthias Bloch <matbloch@student.ethz.ch>
2015-03-17 11:32:24 -0700

    updating package structure & adding examples

--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -19,8 +19,7 @@ public:
     : it_(nh_)
   {
     // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/image_raw", 1, 
-      &ImageConverter::imageCb, this);
+    image_sub_ = it_.subscribe("/camera/image_raw", 1, &ImageConverter::imageCb, this);
     image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
     cv::namedWindow(OPENCV_WINDOW);
