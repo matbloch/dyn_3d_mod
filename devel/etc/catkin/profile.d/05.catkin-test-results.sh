9f1d4fb1422f5f0291820748127f5a3f66199ecf
Matthias Bloch <matbloch@student.ethz.ch>
2015-03-24 04:38:58 -0700

    cleanup

--- devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/mat/ros_workspace/dyn_3d_mod/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
