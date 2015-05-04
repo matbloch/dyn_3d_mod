94bc9fdbd1083b6186f94c51a3a116052cfed834
Eisuke Fujinawa <nawafuji@public-docking-cx-3519.ethz.ch>
2015-05-04 17:11:48 +0200

    edited visualization code

--- src/preprocessing_node_v.cpp
+++ src/preprocessing_node_v.cpp
@@ -56,11 +56,6 @@ voxelGrid grid1;
 voxelGrid grid2;
 
 // globals
-cv::Mat intrinsicMat(3, 3, CV_32F); // intrinsic matrix
-cv::Mat R1(3, 3, CV_32F);    // Rotation vector
-cv::Mat tVec1(3, 1, CV_32F); // Translation vector in camera frame
-cv::Mat R2(3, 3, CV_32F);    // Rotation vector
-cv::Mat tVec2(3, 1, CV_32F); // Translation vector in camera frame
 cv::Mat FilledVoxels1;
 cv::Mat FilledVoxels2;
 cv::Mat FusedVoxels;
@@ -99,17 +94,17 @@ Eigen::MatrixXd V;
 Eigen::MatrixXi F;
 //output: face normals of the reconstructed mesh, #F x3
 Eigen::MatrixXd FN;
-
 // Pointer to the tweak bar
 TwBar *mybar;
 
-
-void getCameraParameters(cv::Mat intrinsicMat);
-void getCameraPose(cv::Mat R, cv::Mat tVec);
+cv::Mat getIntrinsics();
+void getCameraPoses(cv::Mat R1, cv::Mat tVec1, cv::Mat R2, cv::Mat tVec2);
+void InitializeVoxelGrids();
 void print_instructions();
 
 void ros_thread(unsigned int,unsigned int);	    // includes the ros spinner
 void interface_thread();  // user interface
+void InitializeVoxelGrids();
 
 // Visualization Functions
 void TW_CALL ResetGridCB(void *clientData);
@@ -118,7 +113,6 @@ void getLines();
 bool callback_init(igl::Viewer& viewer);
 bool callback_key_down(igl::Viewer& viewer, unsigned char key, int modifiers);
 void visualize_mesh();
-
 
 
 void preprocessing_callback(const sensor_msgs::ImageConstPtr& msg1, const sensor_msgs::ImageConstPtr& msg2)
@@ -160,18 +154,20 @@ void preprocessing_callback(const sensor_msgs::ImageConstPtr& msg1, const sensor
 	filters.bilateral(cv_ptr1->image, filtered1);
 	filters.bilateral(cv_ptr2->image, filtered2);
 
+	// to meters
+	filtered1 = filtered1/1000.0;
+	filtered2 = filtered2/1000.0;
+
+	filters.replaceValue(filtered1, 0, 99);
+	filters.replaceValue(filtered2, 0, 99);
+
 	// set NaNs to 99 m (will be ignored in voxel conversion)
-	cv::patchNaNs(filtered1, 99);
-	cv::patchNaNs(filtered2, 99);
+	//cv::patchNaNs(filtered1, 99);
+	//cv::patchNaNs(filtered2, 99);
 
     /* ========================================== *\
      * 		2. Voxel grid
     \* ========================================== */
-
-
-    // get extrinsics
-//    Eigen::Matrix4f extrinsics;
-//	conf.getOptionMatrix("camera_parameters.extrinsics", extrinsics);
 
 	grid1.fillVoxels(filtered1, FilledVoxels1);
 	grid2.fillVoxels(filtered2, FilledVoxels2);
@@ -192,6 +188,7 @@ void preprocessing_callback(const sensor_msgs::ImageConstPtr& msg1, const sensor
       max_v[i] = (grid.units).at(GRID_SIZE-1);
       min_v[i] = (grid.units).at(0);
     }
+
   
   	tstree.insert(FilledVoxels1, t);
   	t++;
@@ -221,35 +218,8 @@ int main(int argc, char** argv)
 	// add callback
 	sync.registerCallback(boost::bind(&preprocessing_callback, _1, _2));
 
-	// Set up the voxel grid objects for both cameras
-	getCameraPose(R1,tVec1);
-
-	// get extrinsics
-	Eigen::Matrix4f extrinsics;
-	conf.getOptionMatrix("camera_parameters.extrinsics", extrinsics);
-	cv::Mat extrinsics_mat(4,4, CV_32F);
-	cv::eigen2cv(extrinsics, extrinsics_mat);
-	cv::Mat R_ext = extrinsics_mat(cv::Range(0,3), cv::Range(0,3));
-	cv::Mat t_ext = extrinsics_mat(cv::Range(0,3), cv::Range(3,4));
-
-	// get intrinsics
-	Eigen::Matrix3f intrinsics_eig;
-	conf.getOptionMatrix("camera_parameters.intrinsics", intrinsics_eig);
-	cv::eigen2cv(intrinsics_eig, intrinsicMat);
-
-	// Set up the voxel grid objects for both cameras
-	getCameraPose(R1,tVec1);
-	R2 = R_ext*R1;
-	tVec2 = t_ext + R_ext*tVec1;
-
-	grid1.setParameters(GRID_SIZE, spacing_in_m, intrinsicMat, R1, tVec1);
-	grid2.setParameters(GRID_SIZE, spacing_in_m, intrinsicMat, R2, tVec2);
-
-	// Setup voxel structure to load the TSDF in
-	int sz[3] = {GRID_SIZE,GRID_SIZE,GRID_SIZE};
-	FilledVoxels1 = Mat(3,sz, CV_32FC1, Scalar::all(0));
-	FilledVoxels2 = Mat(3,sz, CV_32FC1, Scalar::all(0));
-	FusedVoxels = Mat(3,sz, CV_32FC1, Scalar::all(0));
+	// Configure the voxel grid objects
+	InitializeVoxelGrids();
 
 	ROS_INFO("Preprocessing node initalized.");
 	print_instructions();
@@ -267,35 +237,60 @@ int main(int argc, char** argv)
 }
 
 
-
-void getCameraParameters(cv::Mat intrinsicMat)
-{
-	intrinsicMat.at<float>(0, 0) = 589.3667;  // 640/2/tand(57/2)
-	intrinsicMat.at<float>(1, 0) = 0;
-	intrinsicMat.at<float>(2, 0) = 0;
-
-	intrinsicMat.at<float>(0, 1) = 0;
-	intrinsicMat.at<float>(1, 1) = 609.2755;  // 480/2/tand(43/2)
-	intrinsicMat.at<float>(2, 1) = 0;
-
-	intrinsicMat.at<float>(0, 2) = 319.5;  // 640/2
-	intrinsicMat.at<float>(1, 2) = 239.5;  // 240/2
-	intrinsicMat.at<float>(2, 2) = 1;
-}
-
-void getCameraPose(cv::Mat R, cv::Mat tVec)
-{
+void getCameraPoses(cv::Mat R1, cv::Mat tVec1, cv::Mat R2, cv::Mat tVec2){
+	// Set up the voxel grid position for camera 1
 	cv::Mat rVec(3, 1, cv::DataType<float>::type); // Rotation vector
 	// Rotation vector in Rodrigues angles
 	rVec.at<float>(0) = 0;
 	rVec.at<float>(1) = 0;
 	rVec.at<float>(2) = 0;
-	Rodrigues(rVec, R);
+	Rodrigues(rVec, R1);
 
 	// Translation vector in Camera frame
-	tVec.at<float>(0) = 0;
-	tVec.at<float>(1) = 0;
-	tVec.at<float>(2) = -5;
+	tVec1.at<float>(0) = 0;
+	tVec1.at<float>(1) = 0;
+	tVec1.at<float>(2) = -5;
+
+	// get extrinsics
+	Eigen::Matrix4f extrinsics;
+	conf.getOptionMatrix("camera_parameters.extrinsics", extrinsics);
+	cv::Mat extrinsics_mat(4,4, CV_32F);
+	cv::eigen2cv(extrinsics, extrinsics_mat);
+	cv::Mat R_ext = extrinsics_mat(cv::Range(0,3), cv::Range(0,3));
+	cv::Mat t_ext = extrinsics_mat(cv::Range(0,3), cv::Range(3,4));
+
+	// Set up the voxel grid objects for both cameras
+	R2 = R_ext*R1;
+	tVec2 = t_ext + R_ext*tVec1;
+}
+
+cv::Mat getIntrinsics(){
+	cv::Mat intrinsicMat(3, 3, CV_32F); // intrinsic matrix
+	Eigen::Matrix3f intrinsics_eig;
+	conf.getOptionMatrix("camera_parameters.intrinsics", intrinsics_eig);
+	cv::eigen2cv(intrinsics_eig, intrinsicMat);
+	return intrinsicMat;
+}
+
+void InitializeVoxelGrids(){
+	// Configure rotations and translations
+	cv::Mat R1(3, 3, CV_32F);    // Rotation vector
+	cv::Mat tVec1(3, 1, CV_32F); // Translation vector in camera frame
+	cv::Mat R2(3, 3, CV_32F);    // Rotation vector
+	cv::Mat tVec2(3, 1, CV_32F); // Translation vector in camera frame
+	getCameraPoses(R1,tVec1,R2,tVec2);
+
+	// get intrinsics
+	cv::Mat intrinsicMat = getIntrinsics();
+
+	grid1.setParameters(GRID_SIZE, spacing_in_m, intrinsicMat, R1, tVec1);
+	grid2.setParameters(GRID_SIZE, spacing_in_m, intrinsicMat, R2, tVec2);
+
+	// Setup voxel structure to load the TSDF in
+	int sz[3] = {GRID_SIZE,GRID_SIZE,GRID_SIZE};
+	FilledVoxels1 = Mat(3,sz, CV_32FC1, Scalar::all(0));
+	FilledVoxels2 = Mat(3,sz, CV_32FC1, Scalar::all(0));
+	FusedVoxels = Mat(3,sz, CV_32FC1, Scalar::all(0));
 }
 
 
